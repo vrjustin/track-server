728a1d59f03266dfa10d799c7e316e76e44cdbc1
Justin Maronde <justin.maronde@cengage.com>
2021-06-25 07:52:33 -0400

    176 - configured a new cluster on mongodb.cloud and added the connection string to our index.js and using mongoose to aid in the connection to the mongo server.
    to start server up: node src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const  mongoUri = 'mongodb+srv://admin:CA0Azen28NUFQzN6@cluster0.pwo1h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to Mongo Instance!');
+})
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to Mongo.', err);
+})
 
 app.get('/', (req, res) => {
     res.send('Hi There!');
