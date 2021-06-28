e7cd9b4733d3abee9123f814808d142515197bb2
Justin Maronde <justin.maronde@cengage.com>
2021-06-28 07:05:32 -0400

    wrap our user.save in a try/catch and display the error in the case there is one. We want to return from the server if there is an error - no further processing should occur.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,10 +6,15 @@ const router = express.Router();
 
 router.post('/signup', async (req, res) => {
     const { email, password } = req.body;
-    const user = new User({ email: email, password: password });
-    await user.save();
 
-    res.send('You made a post request');
+    try {
+        const user = new User({ email: email, password: password });
+        await user.save();
+    
+        res.send('You made a post request');    
+    } catch (err) {
+        return res.status(422).send(err.message);
+    }
 });
 
 module.exports = router;
