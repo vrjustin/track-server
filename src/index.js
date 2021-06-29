7604e28a0ecb8066a36c3d4b1149fe269a7a27d3
Justin Maronde <justin.maronde@cengage.com>
2021-06-29 09:03:23 -0400

    creating a new route for the express app so we can get /tracks so create the trackRoutes.js file. RequireAuth for interaction with the route. Tell the new router to user the requireAuth.
    use the .find method on the model so Track.find and pass in the userId to search tracks for. you can use the passed in req objec since it will have ther user attached after we go through the requireAuth middleware.
    
    Export the /tracks router to the rest of the express app.
    
    Then to make the /tracks route available import now to the index.js and ell the app to .use it .use(trackRoutes).

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 require('./models/User');
+require('./models/Track');
 
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const  mongoUri = 'mongodb+srv://admin:CA0Azen28NUFQzN6@cluster0.pwo1h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
